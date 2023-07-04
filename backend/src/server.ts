7922383f4411f02bda8a2e3adade3929ac9d0928
Antonio <netoifpe@gmail.com>
2023-07-04 18:48:35 -0300

    feat: configuring the typeorm and the database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,22 @@
+// import "dotenv/config";
+// import app from "./app";
+
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`App is running on http://localhost:${PORT}`);
+// });
+
 import "dotenv/config";
 import app from "./app";
+import { AppDataSource } from "./data-source";
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Database connected.");
+
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`App is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
