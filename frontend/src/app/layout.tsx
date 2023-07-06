3c75a080805c3ef698981492331dbc2fe761f8ad
Antonio <netoifpe@gmail.com>
2023-07-06 09:32:04 -0300

    feat: creates the header and footer components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header className="w-full border-b-2 border-gray-300">
+      <div
+        className="mx-auto my-0 h-28 w-full max-w-[1200px] flex flex-col items-center 
+      justify-around p-3 sm:h-20 sm:flex-row sm:justify-between"
+      >
+        <Link
+          href={"/"}
+          className="text-4xl bg-gradient-to-r from-green-500 via-cyan-500 
+          to-blue-500 text-transparent bg-clip-text font-bold "
+        >
+          Jet Test
+        </Link>
+
+        <div className="flex gap-4">
+          <Link
+            href={"/"}
+            className="transition ease-in-out delay-150  text-gray-800 hover:text-gray-700
+          hover:-translate-y-1 hover:scale-110 hover:underline hover:underline-offset-2
+           duration-300"
+          >
+            Operadores
+          </Link>
+          <Link
+            href={"/"}
+            className="transition ease-in-out delay-150  text-gray-800 hover:text-gray-700
+            hover:-translate-y-1 hover:scale-110 hover:underline hover:underline-offset-2
+             duration-300"
+          >
+            Clientes
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
